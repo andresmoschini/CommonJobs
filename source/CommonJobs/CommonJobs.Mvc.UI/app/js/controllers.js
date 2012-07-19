97bcfc9c0463bbb2adab66f72133f583c7f2d7a6
Andres Moschini <andresmoschini@gmail.com>
2012-07-19 09:04:15 -0300

    Filter results by term

--- source/CommonJobs/CommonJobs.Mvc.UI/app/js/controllers.js
+++ source/CommonJobs/CommonJobs.Mvc.UI/app/js/controllers.js
@@ -2,11 +2,29 @@
 
 /* Controllers */
 
-function EmployeeListCtrl($scope, Employee) {
-    $scope.employees = Employee.query(function (results) {
-        $scope.employees = results.Items;
+function EmployeeListCtrl($scope, Employee, $location) {
+    $scope.terms = $location.search().term;
+    
+    $scope.$watch('term', function (value) {
+        $location.search("term", value ? value : null).replace().preventLocationChangedEvent();
+        search();
     });
 
+    var currentQuery = null;
+    function search() {
+        //TODO: cancel previous searchs
+        var parameters = {};
+        if ($scope.term) {
+            parameters.Term = $scope.term;
+        }
+        if (currentQuery) {
+            //window.currentQuery = currentQuery;
+            //currentQuery.reject();
+        }
+        window.currentQuery = currentQuery = Employee.query(parameters, function (results) {
+            $scope.employees = results.Items; 
+        });
+    }
 }
 
 
