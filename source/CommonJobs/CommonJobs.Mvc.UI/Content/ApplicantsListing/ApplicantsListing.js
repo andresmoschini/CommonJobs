e2b7c8cf1eecf7afd20380e81e8543c54776157f
Andres Moschini <andresmoschini@gmail.com>
2012-08-07 12:16:34 -0300

    Playing with jquery.fileupload

--- source/CommonJobs/CommonJobs.Mvc.UI/Content/ApplicantsListing/ApplicantsListing.js
+++ source/CommonJobs/CommonJobs.Mvc.UI/Content/ApplicantsListing/ApplicantsListing.js
@@ -1,4 +1,32 @@
-﻿$(function () {
+﻿/// <reference path="../../Scripts/underscore.js" />
+$(function () {
+    $(".mainSearch").bind('drop dragover', function (e) {
+        e.preventDefault();
+    });
+
+    $('#fileupload').fileupload({
+        //no envía el nombre de archivo en IE
+        dataType: 'json',
+        //singleFileUploads: false, //si no se pone esta linea se agrega un add por cada archivo
+        dropZone: $(".mainSearch"),
+        dragover: function (e, f) {
+            if (_.any(e.dataTransfer.types, function (x) { return x == "Files" })) {
+                console.debug("arrastrando un archivo");
+            }
+        },
+        /*
+        add: function (e, data) {
+            var files = _.map(data.files, function (file) { return file.name; });
+            if (files.length > 0 && confirm("Upload files " + files.join(", ") + "?")) {
+                data.submit();
+            }
+        },
+        */
+        done: function (e, data) {
+            //console.debug(data.result);
+        }
+    });
+
     var qs = new QuickSearchPage({
         //pageSize: 3,
         generateRedirectUrl: function (searchParameters) {
