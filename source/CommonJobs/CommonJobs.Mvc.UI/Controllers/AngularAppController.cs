bdfb6015712c3a245ee14c0286f5c77782034603
Andres Moschini <andresmoschini@gmail.com>
2012-08-13 13:31:05 -0300

    Little change in order to fix Raven Profiler button

--- source/CommonJobs/CommonJobs.Mvc.UI/Controllers/AngularAppController.cs
+++ source/CommonJobs/CommonJobs.Mvc.UI/Controllers/AngularAppController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CommonJobs.Raven.Mvc;
 
 namespace CommonJobs.Mvc.UI.Controllers
 {
-    public class AngularAppController : Controller
+    public class AngularAppController : CommonJobsController
     {
         //
         // GET: /app/
