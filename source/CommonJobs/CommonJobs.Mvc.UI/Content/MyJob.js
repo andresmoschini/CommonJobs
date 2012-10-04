a70cdf1c7adf58ceb01afe1faafc9e0bfcd7b75d
Andres Moschini <andresmoschini@gmail.com>
2012-10-04 08:44:05 -0300

    Dynamic data POC

---
+++ source/CommonJobs/CommonJobs.Mvc.UI/Content/MyJob.js
@@ -0,0 +1,35 @@
+﻿/// <reference path="../Scripts/jquery-1.7.2-vsdoc.js" />
+$(function () {
+    window.postBase = function (id, value) {
+        $.ajax({
+            url: "/MyJob/Post/" + id,
+            type: "POST",
+            dataType: "json",
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(value)
+        });
+    };
+
+    
+    window.post = function (id, document, others) {
+        //TODO: document is a string to avoid issues on binding parameters, it could be fixed in a future
+        window.postBase(id, $.extend({}, others, { dynamic: JSON.stringify(document) }));
+    }
+
+    window.get = function (id) {
+        $.ajax({
+            url: "/MyJob/Get/" + id,
+            dataType: "json",
+            success: function(data) {
+                console.debug(data);
+            }
+        });
+    }
+
+    alert("Post a dynamic document...");
+    post("employees/276", { dato1: "hola2", dato2: 5, dato3: { a: "a", b: "b", c: 5 }, dato4: [1, 2, 3, 4, "a", {}] });
+    alert("Posting, wait few seconds. And then, get a dynamic document...");
+    get("employees/276")
+    alert("Done!");
+
+});
---
+++ source/CommonJobs/CommonJobs.Mvc.UI/Controllers/MyJobController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommonJobs.Domain;
+using CommonJobs.Infrastructure;
+using CommonJobs.Infrastructure.Vacations;
+using CommonJobs.Raven.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NLog;
+using Raven.Abstractions.Linq;
+using Raven.Json.Linq;
+
+namespace CommonJobs.Mvc.UI.Controllers
+{
+    public class MyJobController : CommonJobsController
+    {
+        private static Logger log = LogManager.GetCurrentClassLogger();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        private dynamic MyJobResult(string id)
+        {
+            var documentId = GetMyJobDocumentId(id);
+
+            //It is to make only one request
+            RavenSession.Load<dynamic>(id, documentId);
+
+            //TODO: create a view model and a mapping
+            var employee = RavenSession.Load<Employee>(id);
+            var readOnlyData = new { employee.FirstName, employee.LastName };
+
+            var document = RavenSession.Load<dynamic>(documentId);
+
+            return new { employee = readOnlyData, document = document };
+        }
+
+        /// <param name="id">Employee id</param>
+        public ActionResult Get(string id)
+        {
+            //TODO: check if id is allowed for current user
+            return Json(MyJobResult(id));
+        }
+
+        /// <param name="id">EmployeeId Document id, for example employees/243/myjob</param>
+        [HttpPost]
+        public ActionResult Post(string id, string dynamic)
+        {
+            //TODO: check if id is allowed for current user
+            var documentId = GetMyJobDocumentId(id);
+
+            //TODO: document is a string to avoid issues on binding parameters, it could be fixed in a future
+            dynamic document = JsonConvert.DeserializeObject(dynamic, new JsonSerializerSettings() { TypeNameHandling = TypeNameHandling.Auto });
+
+            RavenSession.Store(document, documentId);
+
+            return Json(MyJobResult(id));
+        }
+
+        private static string GetMyJobDocumentId(string id)
+        {
+            var documentId = id + "/myjob";
+            return documentId;
+        }
+    }
+}
