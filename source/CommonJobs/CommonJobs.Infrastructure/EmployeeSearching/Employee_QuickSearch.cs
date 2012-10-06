7373bff6803e05b69e5179cff4346379b332f73b
Andres Moschini <andresmoschini@gmail.com>
2012-10-06 13:07:08 -0300

    pruebas

--- source/CommonJobs/CommonJobs.Infrastructure/EmployeeSearching/Employee_QuickSearch.cs
+++ source/CommonJobs/CommonJobs.Infrastructure/EmployeeSearching/Employee_QuickSearch.cs
@@ -26,21 +26,21 @@ namespace CommonJobs.Infrastructure.EmployeeSearching
             public string Id { get; set; }
             public string FirstName { get; set; }
             public string LastName { get; set; }
-            public string Skills { get; set; }
-            public string FullName1 { get; set; }
-            public string FullName2 { get; set; }
+            //public string Skills { get; set; }
+            //public string FullName1 { get; set; }
+            //public string FullName2 { get; set; }
             public string Terms { get; set; }
-            public string FileId { get; set; }
-            public string Platform { get; set; }
-            public string CurrentPosition { get; set; }
-            public string CurrentProject { get; set; }
+            //public string FileId { get; set; }
+            //public string Platform { get; set; }
+            //public string CurrentPosition { get; set; }
+            //public string CurrentProject { get; set; }
             public string Notes { get; set; }
-            public ImageAttachment Photo { get; set; } 
+            //public ImageAttachment Photo { get; set; } 
 
             public string[] AttachmentIds { get; set; }
             public string[] AttachmentNames { get; set; }
             public string[] AttachmentContent { get; set; }
-            public SlotWithAttachment[] AttachmentsBySlot { get; set; }
+            //-/public SlotWithAttachment[] AttachmentsBySlot { get; set; }
 
             public bool IsEmployee { get; set; }
             public OrphanAttachment[] OrphanAttachments { get; set; }
@@ -55,23 +55,33 @@ namespace CommonJobs.Infrastructure.EmployeeSearching
                     Id = employee.Id,
                     FirstName = employee.FirstName,
                     LastName = employee.LastName,
-                    Skills = employee.Skills,
-                    FullName1 = string.Format("{0}, {1}", employee.LastName, employee.FirstName),
-                    FullName2 = string.Format("{0} {1}", employee.FirstName, employee.LastName),
+                    //Skills = employee.Skills,
+                    //FullName1 = string.Format("{0}, {1}", employee.LastName, employee.FirstName),
+                    //FullName2 = string.Format("{0} {1}", employee.FirstName, employee.LastName),
                     AttachmentIds = employee.AllAttachmentReferences.Select(x => x.Attachment.Id).ToArray(),
                     AttachmentNames = employee.AllAttachmentReferences.Select(x => x.Attachment.FileName).ToArray(),
                     AttachmentContent = new string[0],
-                    AttachmentsBySlot = employee.AttachmentsBySlot,
+                    //-/AttachmentsBySlot = employee.AttachmentsBySlot,
                     IsEmployee = true,
                     OrphanAttachments = new dynamic[0], 
-                    FileId = employee.FileId,
-                    Platform = employee.Platform,
-                    CurrentPosition = employee.CurrentPosition,
-                    CurrentProject = employee.CurrentProject,
+                    //FileId = employee.FileId,
+                    //Platform = employee.Platform,
+                    //CurrentPosition = employee.CurrentPosition,
+                    //CurrentProject = employee.CurrentProject,
                     Notes = employee.Notes,
-                    Photo = employee.Photo,
+                    //Photo = employee.Photo,
                     Terms = new object[]
                     {
+                        employee.FirstName,
+                        employee.LastName,
+                        employee.Skills,
+                        string.Format("{0}, {1}", employee.LastName, employee.FirstName),
+                        string.Format("{0} {1}", employee.FirstName, employee.LastName),
+                        employee.FileId,
+                        employee.Platform,
+                        employee.CurrentPosition,
+                        employee.CurrentProject,
+                        employee.Photo,
                         string.Format("{0:yyyy-MM-dd}", employee.HiringDate),
                         string.Format("{0:dd-MM-yyyy}", employee.HiringDate),
                         string.Format("{0:MM-dd-yyyy}", employee.HiringDate),
@@ -97,20 +107,20 @@ namespace CommonJobs.Infrastructure.EmployeeSearching
                     Id = attachment.RelatedEntityId,
                     FirstName = (string)null,
                     LastName = (string)null,
-                    Skills = (string)null,
-                    FullName1 = (string)null,
-                    FullName2 = (string)null,
+                    //Skills = (string)null,
+                    //FullName1 = (string)null,
+                    //FullName2 = (string)null,
                     AttachmentIds = new string[0],
                     AttachmentNames = new string[0],
                     AttachmentContent = new string[0],
-                    AttachmentsBySlot = new dynamic[0],
+                    //-/AttachmentsBySlot = new dynamic[0],
                     IsEmployee = false,
-                    FileId = (string)null,
-                    Platform = (string)null,
-                    CurrentPosition = (string)null,
-                    CurrentProject = (string)null,
+                    //FileId = (string)null,
+                    //Platform = (string)null,
+                    //CurrentPosition = (string)null,
+                    //CurrentProject = (string)null,
                     Notes = new object[0],
-                    Photo = (object)null,
+                    //Photo = (object)null,
                     Terms = new object[0],
                     OrphanAttachments = new[] { new { Id = attachment.Id, FileName = attachment.FileName, PlainContent = attachment.PlainContent } },
                 });
@@ -123,15 +133,15 @@ namespace CommonJobs.Infrastructure.EmployeeSearching
                     Id = g.Key,
                     FirstName = g.Where(x => x.FirstName != null).Select(x => x.FirstName).FirstOrDefault(),
                     LastName = g.Where(x => x.LastName != null).Select(x => x.LastName).FirstOrDefault(),
-                    Skills = g.Where(x => x.Skills != null).Select(x => x.Skills).FirstOrDefault(),
-                    FullName1 = g.Where(x => x.FullName1 != null).Select(x => x.FullName1).FirstOrDefault(),
-                    FullName2 = g.Where(x => x.FullName2 != null).Select(x => x.FullName2).FirstOrDefault(),
-                    FileId = g.Where(x => x.FileId != null).Select(x => x.FileId).FirstOrDefault(),
-                    Platform = g.Where(x => x.Platform != null).Select(x => x.Platform).FirstOrDefault(),
-                    CurrentPosition = g.Where(x => x.CurrentPosition != null).Select(x => x.CurrentPosition).FirstOrDefault(),
-                    CurrentProject = g.Where(x => x.CurrentProject != null).Select(x => x.CurrentProject).FirstOrDefault(),
+                    //Skills = g.Where(x => x.Skills != null).Select(x => x.Skills).FirstOrDefault(),
+                    //FullName1 = g.Where(x => x.FullName1 != null).Select(x => x.FullName1).FirstOrDefault(),
+                    //FullName2 = g.Where(x => x.FullName2 != null).Select(x => x.FullName2).FirstOrDefault(),
+                    //FileId = g.Where(x => x.FileId != null).Select(x => x.FileId).FirstOrDefault(),
+                    //Platform = g.Where(x => x.Platform != null).Select(x => x.Platform).FirstOrDefault(),
+                    //CurrentPosition = g.Where(x => x.CurrentPosition != null).Select(x => x.CurrentPosition).FirstOrDefault(),
+                    //CurrentProject = g.Where(x => x.CurrentProject != null).Select(x => x.CurrentProject).FirstOrDefault(),
                     Notes = g.SelectMany(x => x.Notes).Distinct().ToArray(),
-                    Photo = g.Where(x => x.Photo != null).Select(x => x.Photo).FirstOrDefault(),
+                    //Photo = g.Where(x => x.Photo != null).Select(x => x.Photo).FirstOrDefault(),
                     Terms = g.SelectMany(x => x.Terms).Distinct().ToArray(),
 
                     AttachmentIds = g.SelectMany(x => x.AttachmentIds).Distinct().ToArray(),
@@ -144,17 +154,17 @@ namespace CommonJobs.Infrastructure.EmployeeSearching
                     ).ToArray(),
                     OrphanAttachments = g.SelectMany(x => x.OrphanAttachments).Where(x => !g.SelectMany(y => y.AttachmentIds).Contains(x.Id)).ToArray(),
 
-                    AttachmentsBySlot = g.SelectMany(x => x.AttachmentsBySlot).Where(x => x != null).ToArray()
+                    //-/AttachmentsBySlot = g.SelectMany(x => x.AttachmentsBySlot).Where(x => x != null).ToArray()
                 };
             
-            Indexes.Add(x => x.FullName1, FieldIndexing.Analyzed);
-            Indexes.Add(x => x.FullName2, FieldIndexing.Analyzed);
-            Indexes.Add(x => x.Skills, FieldIndexing.Analyzed);
+            //Indexes.Add(x => x.FullName1, FieldIndexing.Analyzed);
+            //Indexes.Add(x => x.FullName2, FieldIndexing.Analyzed);
+            //Indexes.Add(x => x.Skills, FieldIndexing.Analyzed);
             Indexes.Add(x => x.AttachmentNames, FieldIndexing.Analyzed);
             Indexes.Add(x => x.AttachmentContent, FieldIndexing.Analyzed);
-            Indexes.Add(x => x.Platform, FieldIndexing.Analyzed);
-            Indexes.Add(x => x.CurrentPosition, FieldIndexing.Analyzed);
-            Indexes.Add(x => x.CurrentProject, FieldIndexing.Analyzed);
+            //Indexes.Add(x => x.Platform, FieldIndexing.Analyzed);
+            //Indexes.Add(x => x.CurrentPosition, FieldIndexing.Analyzed);
+            //Indexes.Add(x => x.CurrentProject, FieldIndexing.Analyzed);
             Indexes.Add(x => x.Notes, FieldIndexing.Analyzed);
             Indexes.Add(x => x.Terms, FieldIndexing.Analyzed);
 		}
