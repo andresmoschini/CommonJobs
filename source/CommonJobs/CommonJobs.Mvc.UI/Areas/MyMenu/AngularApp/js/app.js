ad6b5926c8dc82d8c1b00c74104758e07d51df99
Andrés Moschini <andresmoschini@gmail.com>
2013-03-02 16:45:44 -0300

    Frustration trying locationProvider.html5Mode

--- source/CommonJobs/CommonJobs.Mvc.UI/Areas/MyMenu/AngularApp/js/app.js
+++ source/CommonJobs/CommonJobs.Mvc.UI/Areas/MyMenu/AngularApp/js/app.js
@@ -3,11 +3,14 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('myMenu', ['myMenu.filters', 'myMenu.services', 'myMenu.directives']).
-  config(['$routeProvider', function ($routeProvider) {
+  config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
       //TODO: fix absolute path
       $routeProvider.when('/', { templateUrl: '/Areas/MyMenu/AngularApp/partials/Edit.html', controller: EditCtrl });
       $routeProvider.when('/Edit/:username', { templateUrl: '/Areas/MyMenu/AngularApp/partials/Edit.html', controller: EditCtrl });
       $routeProvider.when('/Admin', { templateUrl: '/Areas/MyMenu/AngularApp/partials/Admin.html', controller: AdminCtrl });
       $routeProvider.when('/Order', { templateUrl: '/Areas/MyMenu/AngularApp/partials/Order.html', controller: OrderCtrl });
       $routeProvider.otherwise({ redirectTo: '/' });
+      //It does not work well because it is assuming that the App is in domain root, 
+      //Is there another configuration?
+      //$locationProvider.html5Mode(true);
   } ]);
